d773d321ece7ce06473def60eeb68002b741f59c
mykola.penkovskyi <mykola.penkovskyi@gmail.com>
2021-09-26 13:37:47 +0200

    add text fields and styles

--- app/src/main/java/eu/holker/builders/screens/distance/Distance.kt
+++ app/src/main/java/eu/holker/builders/screens/distance/Distance.kt
@@ -2,14 +2,20 @@ package eu.holker.builders.screens.distance
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun Distance() {
     val viewModel = DistanceVM()
@@ -17,9 +23,29 @@ fun Distance() {
     Box(
         modifier = Modifier
             .fillMaxSize(), contentAlignment = Alignment.Center
-    ){
+    ) {
+        var distanceString by remember { mutableStateOf("") }
+        var stepString by remember { mutableStateOf("") }
         Column {
-            Button(onClick = { /*TODO*/ }) {
+            Row {
+                TextField(
+                    value = distanceString,
+                    onValueChange = { value -> distanceString = value },
+                    label = { Text("Расстояние в см.") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+            }
+            Row {
+                TextField(
+                    value = stepString,
+                    onValueChange = { value -> stepString = value },
+                    label = { Text("Шаг в см.") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+            }
+            Button(onClick = {
+
+            }) {
                 Text("Расчитать!")
             }
         }
